0b70e281158b48bef0b5f6638c3a170403b86c4a
SailCPU <sailcpu@gmail.com>
2017-03-12 19:16:51 +0800

    Let C4 Robot Move

--- src/RobotDriver.cpp
+++ src/RobotDriver.cpp
@@ -8,20 +8,53 @@ RobotDriver::RobotDriver(QObject *parent)
     //TODO IP and port is written here directly. It will be removed to config file later.
     cmdSendLock= false;
     tcpSocket = new QTcpSocket();
+//    tcpSocket->connect(&QTcpSocket::readyRead,this,&RobotDriver::read_message);
+//    tcpSocket->setParent(0);
+//    // before
+//    tcpSocket->moveToThread( tcpSocketThread );
     connect(tcpSocket, &QTcpSocket::readyRead, this, &RobotDriver::read_message);
     connect(tcpSocket, SIGNAL(error(QAbstractSocket::SocketError)),
             this, SLOT(TCPError(QAbstractSocket::SocketError)));
 }
 void RobotDriver::manager_thread(){
     sendCmd("login");
-    sendCmd("getStatus");
-    executeCmd("moter on");
-    executeCmd("go p3");
+    executeCmd("motor off");
+    sleep(5);
+    executeCmd("motor on");
+    sleep(5);
+    executeCmd("power high");
+    sleep(1);
+    executeCmd("accel 80,80");
+    sleep(1);
+    while(1){
+        sendCmd("reset");
+        executeCmd("power high");
+        sleep(0.2);
+        executeCmd("speed 50");
+        sleep(0.3);
+        executeCmd("accel 50,60");
+        sleep(1);
+        executeCmd("go p1");
+        executeCmd("?speed");
+        sleep(1);
+        executeCmd("go p3");
+        sleep(1);
+        executeCmd("go p2");
+        sleep(1);
+        executeCmd("go p0");
+        sleep(1);
+        executeCmd("go p2");
+        sleep(1);
+        executeCmd("go p0");
+        sleep(1);
+    }
 }
 void RobotDriver::sendCmd(std::string cmd){
     cmd_queue_.push(cmd);
 }
 void RobotDriver::executeCmd(std::string cmd){
+//    sendCmd("reset");
+    sendCmd("getstatus");
     std::string executecmd("execute,\"");
     executecmd.append(cmd).append("\"");;
     sendCmd(executecmd);
@@ -40,7 +73,7 @@ void RobotDriver::send_command_thread(){
         if(!cmd_queue_.empty()&&!cmdSendLock){
             cmdSendLock=true;
             QString package("$");
-            package.append(cmd_queue_.front().data()).append("\n\n");
+            package.append(cmd_queue_.front().data()).append("\t\n");
             cmd_queue_.pop();
             qDebug() << "package" << package;
             tcpSocket->write(package.toStdString().data());
